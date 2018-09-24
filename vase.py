8ee7066f0b3a5b83b1c21893cb6a01f28615c6ef
Joe Cabezas Campos <joe.cabezas@gmail.com>
2018-09-24 02:27:09 -0700

    first commit :)

---
+++ vase.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+from __future__ import division
+import os
+import sys
+import re
+
+# Assumes SolidPython is in site-packages or elsewhwere in sys.path
+from solid import *
+from solid.utils import *
+
+SEGMENTS = 48
+
+def shell(params):
+    height = params['height']
+    min_radius = params['radius']
+    max_radius = min_radius * 1.2
+    twist = params['twist']
+    segments = params['segments']
+    slices = params['slices']
+    
+    shape= circle(r=min_radius, segments=segments)
+    obj = linear_extrude(height=height/2, twist=twist, slices=slices/2, scale=max_radius/min_radius)(shape)
+
+    obj_mirror = up(height)(
+        rotate([180,0,-2*twist])(
+            obj
+        )
+    )
+
+    return obj + obj_mirror
+
+def inside(params):
+    height = params['height']
+    radius = params['radius'] * 0.85
+    twist = params['twist']
+    segments = params['segments']
+
+    shape = cylinder(h=height, r=radius, segments=segments)
+
+    return up(height*0.2)(rotate(-2*twist)(shape))
+
+def assembly():
+    params = {
+        'height' : 45,
+        'radius' : 27,
+        'segments' : 8,
+        'slices' : 2,
+        'twist' : 10
+    }
+
+    return shell(params) - inside(params)
+
+if __name__ == '__main__':
+    use('./list-comprehension-demos/sweep.scad')
+
+    a = assembly()
+    scad_render_to_file(
+        a,
+        'out.scad',
+        file_header='$fn = %s;' % SEGMENTS,
+        include_orig_code=True
+    )
